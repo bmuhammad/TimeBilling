e1a994552ea5e5e844936002a1f477d6e5b98149
bmuhammad <briancmuhammad@yahoo.com>
2025-02-19 14:24:27 -0700

    Added configurations for addng/rendering razor pages. Set up error page for development. Set up path direction for mapping to razor pages.

---
+++ TimeBilling/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TimeBilling.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- TimeBilling/Program.cs
+++ TimeBilling/Program.cs
@@ -1,6 +1,25 @@
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Adds needed dependencies
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (builder.Environment.IsDevelopment())
+{
+    //Sets up error page for development
+    app.UseDeveloperExceptionPage();
+}
+
+//Allows us to server index.html as the default webpage
+app.UseDefaultFiles();
+
+//Allows us to serve files from wwwroot
+app.UseStaticFiles();
+
+//Directs to look under folder structure for razor files
+app.MapRazorPages();
 
 app.Run();
+
